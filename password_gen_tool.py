de66cc50a75e37a3bfd6c8ae5732a6343ef8e1af
CYBORG <79038688+riya78-cyborg@users.noreply.github.com>
2023-10-31 00:14:00 -0700

    Add files via upload

---
+++ password_gen_tool.py
@@ -0,0 +1,19 @@
+import string
+import random
+
+s1 = string.ascii_lowercase
+s2 = string.ascii_uppercase
+s3 = string.digits
+s4 = string.punctuation
+
+s = []
+s.extend(list(s1))
+s.extend(list(s2))
+s.extend(list(s3))
+s.extend(list(s4))
+
+random.shuffle(s)
+
+plength  = int(input("Enter the password length for pass generation: "))
+print("Your new password is : ")
+print("".join(s[0:plength]))
